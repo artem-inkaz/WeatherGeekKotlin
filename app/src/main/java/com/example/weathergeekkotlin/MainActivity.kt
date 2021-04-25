de8dde02ebd22d81b91f196060d5bed158671003
artemternovenko <artem.inkaz@gmail.com>
2021-04-25 20:54:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/weathergeekkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.weathergeekkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
